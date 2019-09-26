84bdd43e7c48937c91d7f7729ce27081ca252ff4
Troy Martin <beef.erikson.studios@gmail.com>
2019-09-26 17:15:38 -0400

    Constants added

--- VariablesAndConstants/constants.cpp
+++ VariablesAndConstants/constants.cpp
@@ -1,19 +1,19 @@
-// Variables Examples
+// Constants example
 // Created by Beef Erikson Studios on 9/26/2019.
 //
 
 /* Uncomment to use - here for build purposes
 
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    int myVar = 0;
-    int yourVar{6};
+    // this can never be changed
+    const int CONSTANT_NUMBER{10};
 
-    cout << myVar << " " << yourVar << endl;
+    cout << CONSTANT_NUMBER << endl;
+
     return 0;
 }
 
