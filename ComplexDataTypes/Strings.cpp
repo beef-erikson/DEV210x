6650bb0b56e6e889b588c2d054f6aecdf13e6aad
Troy Martin <beef.erikson.studios@gmail.com>
2019-09-26 17:38:30 -0400

    Strings added

---
+++ ComplexDataTypes/Strings.cpp
@@ -0,0 +1,34 @@
+// Strings example
+// Created by Beef Erikson Studios on 9/26/2019.
+//
+
+#include <iostream>
+
+using namespace std;
+
+/* Uncomment this to use - here for build
+
+int main()
+{
+    // note we need a termination character '\0'
+    char isAString[6] = { 'H', 'e', 'l', 'l', 'o', '\0'};
+    char isNotAString[5] = { 'H', 'e', 'l', 'l', 'o'};
+
+    cout << isAString << endl;
+    cout << isNotAString << endl << endl;
+
+    // alternate method is to initialize with a string literal
+    char isAnotherString[6] = "Hello";
+    char isYetAnotherString[] = "Array size is inferred";
+
+    cout << isAnotherString << endl;
+    cout << isYetAnotherString << endl << endl;
+
+    // can also use the string class
+    string myString = "Hello!";
+    cout << myString << endl;
+
+    return 0;
+}
+
+*/
