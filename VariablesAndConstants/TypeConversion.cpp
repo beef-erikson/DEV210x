ed0d051a9436f453860b55c9f7a0db26b052f619
Troy Martin <beef.erikson.studios@gmail.com>
2019-09-26 17:24:03 -0400

    TypeConversion added

---
+++ VariablesAndConstants/TypeConversion.cpp
@@ -0,0 +1,29 @@
+// Type Conversions example
+// Created by Beef Erikson Studios on 9/26/2019.
+//
+
+#include <iostream>
+
+using namespace std;
+
+/* Uncomment to use - here for build
+
+int main()
+{
+    // assigning a cast through type
+    int myInt = 12;
+    long myLong;
+    myLong = myInt;
+
+    cout << myLong << endl;
+
+    // can also use these versions
+    long anotherLong = (long)myInt;
+    long yetAnotherLong = long(myInt);
+    long andAnotherLong = static_cast<long>(myInt);
+
+    cout << anotherLong << " " << yetAnotherLong << " " << andAnotherLong << endl;
+    return 0;
+}
+
+*/
