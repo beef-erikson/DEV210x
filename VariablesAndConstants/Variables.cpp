f4c3931cf9de766acec58712a26b651997267de3
Troy Martin <beef.erikson.studios@gmail.com>
2019-09-26 17:00:33 -0400

    Initial commit

---
+++ VariablesAndConstants/Variables.cpp
@@ -0,0 +1,18 @@
+// Variables Examples
+// Created by Beef Erikson Studios on 9/26/2019.
+//
+
+/* Uncomment to use - here for build purposes
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int myVar = 0;
+    int yourVar{6};
+
+    cout << myVar << " " << yourVar << endl;
+    return 0;
+}
+*/
