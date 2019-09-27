49784147b4d814a561ff42a4daf37e76047ebab5
Troy Martin <beef.erikson.studios@gmail.com>
2019-09-27 19:34:01 -0400

    Union added

---
+++ ComplexDataTypes/Union.cpp
@@ -0,0 +1,34 @@
+// Unions example
+// Created by Beef Erikson Studios on 9/27/2019.
+//
+
+#include <iostream>
+
+using namespace std;
+
+/* remove comment to use - here for building
+
+union numericUnion
+{
+    int intValue;
+    long longValue;
+    double doubleValue;
+};
+
+int main()
+{
+    numericUnion myUnion;
+
+    // can only store one piece of data at a time, as demonstrated here.
+    // useful for things like a part number where it may be a string or an int but never both.
+    myUnion.intValue = 3;
+    cout << myUnion.intValue << endl;
+
+    myUnion.doubleValue = 4.5;
+    cout << myUnion.doubleValue << endl;
+    cout << myUnion.intValue << endl;
+
+    return 0;
+}
+
+*/
