82c2b7a6f72e859418b74f1553e0a85ef0be9728
Troy Martin <beef.erikson.studios@gmail.com>
2019-09-27 22:21:10 -0400

    WhileLoops added

---
+++ RepetitionStatements/WhileLoops.cpp
@@ -0,0 +1,29 @@
+// While Loops Example
+// Created by Beef Erikson Studios on 9/27/2019.
+//
+
+#include <iostream>
+
+using namespace std;
+
+/* Uncomment to use - here for build purposes
+
+// simple menu using a while loop
+int main()
+{
+    string response;
+    cout << "Enter menu choice " << endl << "More" << endl << "Quit" << endl;
+    cin >> response;
+
+    while (response != "Quit")
+    {
+        // code to execute if Quit is not entered
+
+        cout << "Enter menu choice " << endl << "More" << endl << "Quit" << endl;
+        cin >> response;
+    }
+
+    return 0;
+}
+
+*/
