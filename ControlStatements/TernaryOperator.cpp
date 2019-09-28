3987e48023b1233a705a5c0bdd02bbe2becbd17d
Troy Martin <beef.erikson.studios@gmail.com>
2019-09-27 21:05:55 -0400

    TernaryOperator added

---
+++ ControlStatements/TernaryOperator.cpp
@@ -0,0 +1,21 @@
+// Conditional (Ternary) Operator Example
+// Created by Beef Erikson Studios on 9/27/2019.
+//
+
+#include <iostream>
+
+using namespace std;
+
+/* Uncomment to use - here for build purposes
+
+int main()
+{
+    int i = 1, j = 2;
+
+    // think of this as an if/else statement.
+    cout << ( i > j ? i : j ) << " is greater." << endl;
+
+    return 0;
+}
+
+*/
