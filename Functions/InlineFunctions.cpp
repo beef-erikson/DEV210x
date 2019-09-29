843c2de4a179d520cb28e69ad8651e2d9c6b81c6
Troy Martin <beef.erikson.studios@gmail.com>
2019-09-28 21:06:44 -0400

    InlineFunctions added

---
+++ Functions/InlineFunctions.cpp
@@ -0,0 +1,31 @@
+// Inline Functions Example
+// Created by Beef Erikson Studios on 9/28/2019.
+//
+
+#include <iostream>
+
+using namespace std;
+
+/* Uncomment to use - here for build purposes
+
+inline void swap (int& a, int& b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main()
+{
+    int a = 5;
+    int b = 8;
+
+    swap(a, b);
+
+    // note the values are swapped
+    cout << "a: " << a << " b: " << b << endl;
+
+    return 0;
+}
+
+*/
