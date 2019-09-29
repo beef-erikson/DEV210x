70f2bf14a80e5bb59144139ab59c302815e97d14
Troy Martin <beef.erikson.studios@gmail.com>
2019-09-28 23:57:58 -0400

    MoreInitializing added

---
+++ IntroducingClasses/MoreInitializing.cpp
@@ -0,0 +1,25 @@
+// More Initializing Examples
+// Created by Beef Erikson Studios on 9/28/2019.
+//
+
+#include "MoreInitializing.h"
+
+/* Uncomment to use - here for build purposes
+
+int main()
+{
+    int x;          // junk
+    int y{};        // 0
+    int z{ 4 };     // 4
+
+    MoreInitializing uninitialized;         // junk
+    MoreInitializing value{};               // 0, 0
+    MoreInitializing aggregate{ 2, 4 };     // 2, 4
+
+    value.m_height = 2;
+    value.m_width = 4;
+
+    return 0;
+}
+
+*/
---
+++ IntroducingClasses/MoreInitializing.h
@@ -0,0 +1,16 @@
+//
+// Created by Beef Erikson Studios on 9/28/2019.
+//
+
+#ifndef DEV210X_MOREINITIALIZING_H
+#define DEV210X_MOREINITIALIZING_H
+
+
+class MoreInitializing {
+public:
+    int m_width;
+    int m_height;
+};
+
+
+#endif //DEV210X_MOREINITIALIZING_H
