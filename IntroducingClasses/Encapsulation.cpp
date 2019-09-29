b43a8f90038047feffffe92efa0afda3b38e6cca
Troy Martin <beef.erikson.studios@gmail.com>
2019-09-29 01:12:10 -0400

    Encapsulation added

---
+++ IntroducingClasses/Encapsulation.cpp
@@ -0,0 +1,30 @@
+// Example of Encapsulation
+// Created by Beef Erikson Studios on 9/29/2019.
+//
+
+#include "Encapsulation.h"
+#include <iostream>
+
+using namespace std;
+
+/* Uncomment to use - here for build purposes
+
+int main()
+{
+    // normally we would call this Rectangle, but for the sake of demo, named this.
+    // see Encapsulation.h for method / constructor definitions
+    Encapsulation rect { 3, 3 };
+    int theArea{rect.get_area()};
+    cout << theArea << endl;
+
+    rect.resize(5, 32);
+    cout << rect.get_area() << endl;
+
+    Encapsulation defaultRect{};
+    cout << "Width: " << defaultRect.get_width() << " Height: " << defaultRect.get_height() << endl;
+    cout << "Area of default rectangle: " << defaultRect.get_area() << endl;
+
+    return 0;
+}
+
+*/
---
+++ IntroducingClasses/Encapsulation.h
@@ -0,0 +1,46 @@
+//
+// Created by Beef Erikson Studios on 9/29/2019.
+//
+
+#ifndef DEV210X_ENCAPSULATION_H
+#define DEV210X_ENCAPSULATION_H
+
+
+class Encapsulation {
+// constructors
+public:
+    // default
+    Encapsulation()
+    : m_width{ 1 }, m_height{ 1 }
+    {}
+
+    // supplied values
+    Encapsulation(int initial_width, int initial_height)
+    : m_width{initial_width}, m_height{initial_height}
+    {}
+
+// methods
+public:
+    int get_area()
+    {
+        // note we don't necessarily need 'this' keyword; here to clarify
+        return this->m_width * this->m_height;
+    }
+
+    void resize(int new_width, int new_height)
+    {
+        m_width = new_width;
+        m_height = new_height;
+    }
+
+    int get_width() { return m_width; }
+    int get_height() { return m_height; }
+
+// values
+private:
+    int m_width;
+    int m_height;
+};
+
+
+#endif //DEV210X_ENCAPSULATION_H
